49986e580cbb8168c252698db51bf9debb2b2ca7
Alan H <sorslyd5@protonmail.com>
2019-05-30 00:25:05 -0500

    added actual run docker gui img script (assuming image has been built locally already...)

--- docker.sh
+++ docker.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-#PyQt, used for the GUI
-pip install PyQt
+winpty docker run -p 5901:5901 -t -i gui-template
 
-#fasthenry, used to compute the actual inductance
-cd ~
-curl wrcad.com/ftp/pub/fasthenry-3.0wr-110915.tar.gz
-
-#NLopt, to find the inductor dimensions
-curl https://github.com/stevengj/nlopt/archive/v2.6.1.tar.gz
-cmake . && make && make install
+./dep.sh
